3dad234f7cbadb6bc0a8db1c556762b0729dec09
michael landreville <mlandreville@planipret.com>
2025-11-26 17:24:53 -0500

    Adding the Cocky AI

--- app/jobs/chat_response_job.rb
+++ app/jobs/chat_response_job.rb
@@ -2,7 +2,7 @@ class ChatResponseJob < ApplicationJob
   def perform(chat_id, content)
     chat = Chat.find(chat_id)
 
-    chat.ask(content) do |chunk|
+   chat.ask(content) do |chunk|
       if chunk.content.present?
         message = chat.messages.last
         message.broadcast_append_chunk(chunk.content)
