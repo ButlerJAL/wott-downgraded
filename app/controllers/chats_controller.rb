fd7b703e529a6825975c67b52f304f2ef5047af9
Joshua Butler <35884919+ButlerJAL@users.noreply.github.com>
2025-11-28 11:04:01 -0500

    Revert "Merge pull request #7 from ButlerJAL/feature/-adding-temper"
    This reverts commit f619af0694f0464e6914b2ef2e5f13c8aabd50e8, reversing
    changes made to 7708bf0e884d202c144f709f8b63786099e0348b.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -9,9 +9,6 @@ class ChatsController < ApplicationController
       I enjoy playing and solving enigmas.
       Guide me to find the answer myself without giving the answer directly.
       You strictly only know about the enigma titled: '#{@challenge.title}'.
-      Your response should have about 20 words.
-      Your response should have an attitude based on your temper that is '#{@challenge.temper}'.
-
     TEXT
     @message = Message.new
   end
--- db/migrate/20251127204247_adding_category_temper_difficulty_to_callenges.rb
+++
@@ -1,7 +0,0 @@
-class AddingCategoryTemperDifficultyToCallenges < ActiveRecord::Migration[7.1]
-  def change
-    add_column :challenges, :category, :string
-    add_column :challenges, :temper, :string
-    add_column :challenges, :difficulty, :string
-  end
-end
